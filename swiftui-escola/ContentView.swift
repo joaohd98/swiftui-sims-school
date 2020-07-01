948581b8319fb2da33d0868b9036b7eaecd08455
João Damazio <joaohd98@gmail.com>
2020-07-01 16:08:42 -0300

    Initial Commit

---
+++ swiftui-escola/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-escola
+//
+//  Created by João Damazio on 01/07/20.
+//  Copyright © 2020 João Damazio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
